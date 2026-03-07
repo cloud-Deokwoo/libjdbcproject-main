6e98893dc73aa2aeaf9827f7ad4bc5ce28d6631d
kdw335552 <kdw335552@nate.com>
2026-03-07 22:49:29 +0900

    정리

--- src/Bookmain.java
+++
@@ -1,8 +0,0 @@
-import controller.BookController;
-
-public class Bookmain {
-    public static void main(String[] args) {
-        BookController controller = new BookController();
-        controller.start();
-    }
-}
